3d1779f4f7527909df183334de72208f4afe55c7
drewcarroll-taller <drew.carroll@tallertechnologies.com>
2026-06-29 14:17:43 -0700

    Delete supabase/migrations directory

--- supabase/migrations/0002_add_times_cooked.sql
+++
@@ -1,4 +0,0 @@
--- Recipe Tracker: track how many times each recipe has been cooked.
-alter table public.recipes
-  add column if not exists times_cooked integer not null default 0
-    check (times_cooked >= 0);
